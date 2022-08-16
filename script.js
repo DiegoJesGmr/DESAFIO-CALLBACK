d29f90e9b79a01474b4a36b3b1215abb97dbedf2
diegojgmr <88859284+diegojgmr@users.noreply.github.com>
2022-08-16 19:13:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let Gastoconiva = function(numero){
+/*let Gastoconiva = function(numero){
     let precio = numero *1.21;
     return precio;
 }
@@ -21,3 +21,35 @@ return callback.og * 1.21
 
 //listanueva = map(miarray); 
 console.log(a_modificado)
+*/
+// CCORRECCIÓN
+function gastoconiva(numero){
+ 
+    return numero * 1.21 ;
+}
+/*function operation(og) {
+og *1.45;
+}*/
+//declaro el array original 
+let miarray = [1,2,3,4];
+/*let a_modificado = miarray.map(function(callback){
+// llamo al array con map, lo revisa y que retorne 
+// un valor modificado
+
+return callback.og * 1.21
+});*/
+
+function map(miarray,gastoconiva){
+   let a_modificado=[];
+   for(let i=0;i<miarray.length;i++){
+    let numero=miarray[i];
+   
+    a_modificado.push(gastoconiva(numero))
+      
+} 
+return a_modificado;
+}
+
+//listanueva = map(miarray); 
+let nuevalista = map(miarray)
+console.log(nuevalista)
